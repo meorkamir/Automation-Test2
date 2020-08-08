56f5656cbb17f27522a55790a0efc1c0212bf4b5
meorkamir <meorkamir@gmail.com>
2020-08-08 16:16:32 +0800

    Installation .bat file

---
+++ installation/install.bat
@@ -0,0 +1,5 @@
+REM Installing required framework
+python -m pip install --upgrade pip
+pip install --upgrade robotframework
+pip install --upgrade robotframework-seleniumlibrary
+pip install beautifulsoup4
